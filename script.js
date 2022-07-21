3dfaebd9d72316a2ec199b91cd0ef95b3d231ef3
Valerie Oakhu <ooakhu@gmail.com>
2022-07-21 22:10:20 +0300

    working on axios

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 let file = document.querySelector( "#file-item" )
+const baseUrl = 'https://bionic-reading1.p.rapidapi.com/convert'
 
-
-file.addEventListener( "change", readFile )
-
-function readFile () {
-    let file = this.files[0]
+const readFile = async function () {
+    let file = document.querySelector( "#file-item" ).files[0]
 
     if ( file.length === 0 ) {
         console.log( "no file was upload" )
     } else {
         console.log( "we got a valid file" )
-        file.text()
-            .then( ( data ) => {
-                console.log( data )
-                return data
-            } )
-
-        // var reader = new FileReader();
-        // reader.onload = function ( event ) {
-        //     console.log( 'File content:', event.target.result );
-        // };
-        // let finalResult = reader.readAsText( file );
+        let result = await file.text()
+        console.log( result )
+        return result
     }
 }
 
+
+const bionicRead = async function () {
+    let book = await readFile()
+    const config = {
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            "X-RapidAPI-Key": "36b8a549c8msheef6311c5717a93p18c557jsnedc848359f16",
+            "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
+        },
+        data: {
+            "content": book,
+            "response_type": "page",
+            "request_type": "html",
+            "fixation": 2,
+            "saccade": 10
+        }
+    }
+
+    let result = await axios.post( baseUrl, config )
+    console.log( result )
+}
+
+
+
+file.addEventListener( "change", bionicRead )
